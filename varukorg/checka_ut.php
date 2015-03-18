fbfb560c8ee62768bbbdc9fad7800abdcaa86a03
mranstrand <michael.ranstrand@gmail.com>
2015-03-18 16:27:49 +0100

    Fixa korgen och genomföra köp

---
+++ varukorg/checka_ut.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: michaelranstrand
+ * Date: 15-03-11
+ * Time: 14:54
+ */
--- varukorg/visa_korg.php
+++ varukorg/visa_korg.php
@@ -8,13 +8,9 @@
 
 session_start();
 
-$cart = $_SESSION["cart"];
-
-
-
 ?>
 
-    <html>
+<html xmlns="http://www.w3.org/1999/html">
     <head>
         <title>Lägg produkt i korg</title>
         <meta charset="utf-8">
@@ -25,17 +21,48 @@ $cart = $_SESSION["cart"];
 
 <?php
 
+//Variabler för databaskoppling
+$dbhost     = "localhost";
+$dbname     = "diyShop";
+$dbuser     = "root";
+$dbpass     = "";
+
+//Koppla till databasen
+$DBH = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
+
+// Välj felhantering (här felsökningsläge)
+$DBH->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
 // Loopa igenom produkter i varukorg
-foreach ($cart as $product){
+foreach ($_SESSION["cart"] as $product){
 
-    echo  "<br>Id = " . $product["id"] . ". antal = " . $product["antal"];
 
+
+// Förbered databasfråga med placeholders (markerade med : i början)
+    $STH = $DBH->prepare("SELECT * FROM tbl_produkter WHERE id = :id");
+
+//Ersätt placeholders med värden från variabler
+    $STH->bindParam(':id', $produkt["produkt_id"]);
+
+//Utför frågan
+    $STH->execute();
+
+//Tar emot värden
+    $row = $STH->fetch();
+?>
+<?php echo $row["titel"]; ?>
+Pris: <?php echo $row["pris"]; ?> kr.
+    Antal: <?php echo $product["antal"]; ?> kr.
+    </br></br>
+
+<?php
 }
+//Stänger databaskopplingen
+$DBH = null;
 
 ?>
 
 <a href="../produkt/produktlista.php">Visa varulista</a>
-
+<a href="checka_ut.php">Genomför köp.</a>
     </body>
 
     </html>
